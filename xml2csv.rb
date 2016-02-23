753dac0c6db3d01ca2a951e1814e269871ba18e7
Sergiu-Alexandru Vidrascu <Sergiu.Vidrascu@ness.com>
2016-02-23 12:28:07 +0200

    beautify

--- xml2csv.rb
+++ xml2csv.rb
@@ -4,10 +4,11 @@ require 'csv'
 
 file = File.read('test.xml')
 doc = Nokogiri::XML(file)
-CSV.open("result.csv", "wb")  do |csv|
-  xml = doc.xpath("//network_object[Class_Name='network']/Name[starts-with(., 'Ness-RO')]/..")
-  xml.each do |magical_element|
-    config = XmlSimple.xml_in(magical_element.to_xml, { 'KeyAttr' => 'name' })
-  csv << [config.to_s.gsub(/\"/,"").gsub(/{/,"").gsub(/}/,"").gsub(/=>\[\]/,"").gsub(/=>/,",").gsub(/\[/,"").gsub(/\]/,"").gsub(/, /,",")]
-  end
+csv = CSV.open("result.csv", "wb")
+
+filtered_xml = doc.xpath("//network_object[Class_Name='network']/Name[starts-with(., 'Ness-RO')]/..")
+filtered_xml.each do |magical_element|
+  element_hash = XmlSimple.xml_in(magical_element.to_xml, { 'KeyAttr' => 'name' })
+  stripped_hash = element_hash.to_s.gsub(/\"/,"").gsub(/{/,"").gsub(/}/,"").gsub(/=>\[\]/,"").gsub(/\[/,"").gsub(/\]/,"").gsub(/, /,",").gsub(/=>/,",")
+csv << [stripped_hash]
 end
